b2a11126d3c93964688e75cebea0d8bf44cd492e
John Kisor <jkisor@gmail.com>
2015-11-29 21:23:06 -0500

    add afk/lock desktop alias

---
+++ install.sh
@@ -0,0 +1,5 @@
+ln -sfv ~/dotfiles/.alias ~/.alias
+ln -sfv ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sfv ~/dotfiles/.bashrc ~/.bashrc
+ln -sfv ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sfv ~/dotfiles/.vimrc ~/.vimrc
